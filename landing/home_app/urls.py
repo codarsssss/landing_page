493dab2fd7f7608734b9760306aa6405732203b2
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-01 17:32:45 +0300

    add index view + forms

---
+++ landing/home_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name = 'home_app'
+
+urlpatterns = [
+    path('', index, name='index_t')
+]
--- landing/home_app/views.py
+++ landing/home_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+
+    context = {
+        'title': 'Hello, World!'
+    }
+
+    return render(request, 'home_app/index.html', context=context)
