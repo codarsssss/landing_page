d1a1c8c3cc31550894a8eac8782de20ff08bb44d
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-02 11:15:18 +0300

    add_news_detail_view

--- landing/home_app/views.py
+++ landing/home_app/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import News, Review
 
-# Create your views here.
 
-
+# Главная страница
 def index(request):
 
+    news = News.objects.filter(active=True)
+    review = Review.objects.filter(active=True)
+
     context = {
-        'title': 'Hello, World!'
+        'title': 'Главная страница',
+        'Review': review,
+        'News': news
     }
 
     return render(request, 'home_app/index.html', context=context)
+
+
+
+# Детальный взгляд на пост
+def news_detail(request, slug):
+
+    try:
+        news_obj = News.object.filter(slug=slug)
+    except News.DoesNotExist:
+        raise Http404('Такой новости нет.')
+
+    context = {
+        'title': 'Новости',
+        'news_obj': news_obj
+    }
+
+    return render(request, 'home_app/news_detail.html', context=context)
