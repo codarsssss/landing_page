49c7058317e21a21da17b7ebbb8039bf9bada227
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-04 11:27:43 +0300

    fix modal logic

--- landing/home_app/static/home_app/js/modal_logic.js
+++ landing/home_app/static/home_app/js/modal_logic.js
@@ -1,21 +1,30 @@
-const btn=document.querySelectorAll('.modal_call');
-const modal=document.querySelector('.modal_block');
-const modal_close_btn =document.querySelector('.close_btn');
+const btn = document.querySelectorAll('.modal_call');
+const modal_block = document.querySelector('.modal_block');
+const modal_wrapper = document.querySelector('.modal');
+const modal_close_btn = document.querySelector('.close_btn');
 
 
 let toggleModal = function toggleModal() {
-    modal.classList.toggle('active');
-}
+    modal_block.classList.toggle('active');
+};
 
 for (let i = 0; i < btn.length; ++i){
     btn[i].addEventListener('click', function (e){
     e.stopPropagation();
     toggleModal();
 })
-}
+};
 
 
 modal_close_btn.addEventListener('click', function (e){
     e.stopPropagation();
     toggleModal();
-})
+});
+
+
+modal_block.addEventListener('click', function(e) {
+    if (e.target === modal_block) {
+        e.stopPropagation();
+        toggleModal();
+    }
+});
