92a2a6acc28f364a6f772a6ec5bed2dd5c4dab08
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-06 12:26:30 +0300

    swiper for reviews and news

--- landing/home_app/static/home_app/js/slider.js
+++ landing/home_app/static/home_app/js/slider.js
@@ -1,8 +1,61 @@
-$('.reviews_block').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 60,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
+  }
 });
 
+
+var swiper = new Swiper(".mySwiper_1", {
+  slidesPerView: 3,
+  spaceBetween: 60,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
+  }
+});
 
