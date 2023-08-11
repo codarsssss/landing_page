0e7483b684f76eb0e6311c3d2db59b9a6bd90feb
codarsssss <codarsssss@gmail.com>
2023-08-11 21:11:26 +0300

    fix news slider

--- landing/home_app/static/home_app/js/slider.js
+++ landing/home_app/static/home_app/js/slider.js
@@ -6,17 +6,14 @@ var swiper = new Swiper(".mySwiper", {
     delay: 1000,
   },
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
       spaceBetween: 20
     },
-    // when window width is >= 480px
     480: {
       slidesPerView: 1,
       spaceBetween: 30
     },
-    // when window width is >= 640px
     720: {
       slidesPerView: 2,
       spaceBetween: 40
@@ -37,20 +34,17 @@ var swiper = new Swiper(".mySwiper_1", {
     delay: 1000,
   },
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
       spaceBetween: 10
     },
-    // when window width is >= 480px
     480: {
       slidesPerView: 1,
       spaceBetween: 30
     },
-    // when window width is >= 640px
     720: {
       slidesPerView: 2,
-      spaceBetween: 80
+      spaceBetween: 40
     },
     920: {
       slidesPerView: 3,
