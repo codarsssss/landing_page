65bca7767adece9dc305d5f2f0f0f72e256c72bb
vvetta <drpsih2004@gmail.com>
2023-08-16 23:27:52 +0300

    add content block for service modal

--- landing/home_app/static/home_app/js/modal_logic.js
+++ landing/home_app/static/home_app/js/modal_logic.js
@@ -6,13 +6,9 @@ const body = document.querySelector('html');
 
 const service_btns = document.querySelectorAll('.modal_call_service');
 const service_items = document.querySelectorAll('.services__item')
-const modal_ids = document.querySelectorAll('.service_modal')
-const service_modal_wrapper = document.querySelectorAll('.service_modal_wrapper');
-const service_modal_close_btn = document.querySelectorAll('.service_modal_close_btn');
-
 const tonnerBlock = document.querySelector('.tonner_block');
 
-console.log(modal_ids)
+
 let toggleModal = function toggleModal() {
 
     modal_block.classList.toggle('active');
@@ -60,6 +56,7 @@ for (let i = 0; i < service_btns.length; i++) {
         service_items[i].style.transform = 'scale(1)';
         service_items[i].style.position = 'relative';
         service_items[i].style.boxShadow = 'none'
+        service_btns[i].style.display = 'inline'
         body.style.overflowY = 'visible';
     })
 
@@ -75,23 +72,10 @@ for (let i = 0; i < service_btns.length; i++) {
         service_items[i].style.transform = 'translate(50%, -50%)';
         service_items[i].style.transform = 'scale(1.05)';
         service_items[i].style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px';
-        // modal_ids[i].style.opacity = '1';
-        // modal_ids[i].style.zIndex = '1001';
-        // service_modal_wrapper[i].style.zIndex = '1002';
-        // service_modal_wrapper[i].style.top = '0';
-        // service_modal_wrapper[i].style.opacity = '1';
+        service_btns[i].style.display = 'none'
+      
         body.style.overflowY = 'hidden';
         })
-
-    service_modal_close_btn[i].addEventListener('click', function(e){
-        e.stopPropagation();
-        // modal_ids[i].style.opacity = '0';
-        // modal_ids[i].style.zIndex = '-100';
-        // service_modal_wrapper[i].style.zIndex = '-100';
-        // service_modal_wrapper[i].style.top = '-100px';
-        // service_modal_wrapper[i].style.opacity = '0';
-        // body.style.overflowY = 'visible';
-    })
 }
 
 
