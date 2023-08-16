1dcf01938d26dfb22b6a8ef6b334580f7783b458
Nikita <109899745+vvetta@users.noreply.github.com>
2023-08-17 02:07:11 +0300

    fix news img and services modal

--- landing/home_app/static/home_app/js/modal_logic.js
+++ landing/home_app/static/home_app/js/modal_logic.js
@@ -5,7 +5,8 @@ const modal_close_btn = document.querySelector('.close_btn');
 const body = document.querySelector('html');
 
 const service_btns = document.querySelectorAll('.modal_call_service');
-const service_items = document.querySelectorAll('.services__item')
+const service_items = document.querySelectorAll('.services__item');
+const services__item_content = document.querySelectorAll('.services__item_content');
 const tonnerBlock = document.querySelector('.tonner_block');
 
 
@@ -46,7 +47,14 @@ modal_block.addEventListener('click', function(e) {
 
 for (let i = 0; i < service_btns.length; i++) {
 
+    let toggleServicesContent = function toggleServicesContent(i) {
+        services__item_content[i].classList.toggle("active");
+    }
+
     tonnerBlock.addEventListener('click', function(e) {
+        if (services__item_content[i].classList.contains = 'active') {
+            services__item_content[i].classList.remove('active');
+        }
         e.stopPropagation();
         tonnerBlock.classList.remove('active');
         service_items[i].style.top = '0px';
@@ -55,8 +63,8 @@ for (let i = 0; i < service_btns.length; i++) {
         service_items[i].style.zIndex = '1';
         service_items[i].style.transform = 'scale(1)';
         service_items[i].style.position = 'relative';
-        service_items[i].style.boxShadow = 'none'
-        service_btns[i].style.display = 'inline'
+        service_items[i].style.boxShadow = 'none';
+        service_btns[i].style.display = 'inline';
         body.style.overflowY = 'visible';
     })
 
@@ -73,7 +81,7 @@ for (let i = 0; i < service_btns.length; i++) {
         service_items[i].style.transform = 'scale(1.05)';
         service_items[i].style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px';
         service_btns[i].style.display = 'none'
-      
+        toggleServicesContent(i);
         body.style.overflowY = 'hidden';
         })
 }
